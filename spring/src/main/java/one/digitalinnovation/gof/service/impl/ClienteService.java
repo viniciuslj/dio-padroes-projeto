25881f2c47f2fbe5545e04992f2545db7d144920
Vinicius Louzada Jacobsem <vinicius.lj@gmail.com>
2022-01-30 21:27:08 -0300

    (Spring) Service

---
+++ spring/src/main/java/one/digitalinnovation/gof/service/impl/ClienteService.java
@@ -0,0 +1,62 @@
+package one.digitalinnovation.gof.service.impl;
+
+import java.util.Optional;
+import lombok.Data;
+import org.springframework.stereotype.Service;
+import one.digitalinnovation.gof.model.Cliente;
+import one.digitalinnovation.gof.model.ClienteRepository;
+import one.digitalinnovation.gof.model.Endereco;
+import one.digitalinnovation.gof.model.EnderecoRepository;
+import one.digitalinnovation.gof.service.IClienteService;
+import one.digitalinnovation.gof.service.IViaCepService;
+
+@Service
+@Data
+public class ClienteService implements IClienteService {
+
+	private final ClienteRepository clienteRepository;
+	private final EnderecoRepository enderecoRepository;
+	private final IViaCepService viaCepService;
+
+	@Override
+	public Iterable<Cliente> buscarTodos() {
+		return clienteRepository.findAll();
+	}
+
+	@Override
+	public Cliente buscarPorId(Long id) {
+		Optional<Cliente> cliente = clienteRepository.findById(id);
+		return cliente.get();
+	}
+
+	@Override
+	public void inserir(Cliente cliente) {
+		salvarClienteComCep(cliente);
+	}
+
+	@Override
+	public void atualizar(Long id, Cliente cliente) {
+		Optional<Cliente> clienteBd = clienteRepository.findById(id);
+		if (clienteBd.isPresent()) {
+			salvarClienteComCep(cliente);
+		}
+	}
+
+	@Override
+	public void deletar(Long id) {
+		clienteRepository.deleteById(id);
+	}
+
+	private void salvarClienteComCep(Cliente cliente) {
+		String cep = cliente.getEndereco().getCep();
+
+		Endereco endereco = enderecoRepository.findById(cep).orElseGet(() -> {
+			Endereco novoEndereco = viaCepService.consultarCep(cep);
+			enderecoRepository.save(novoEndereco);
+			return novoEndereco;
+		});
+
+		cliente.setEndereco(endereco);
+		clienteRepository.save(cliente);
+	}
+}
